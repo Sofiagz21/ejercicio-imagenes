b22ee70f6fd9ed24683771dbcf1bd81648723489
SofiaGonzalez <sofi7zubieta@gmail.com>
2021-06-11 08:23:55 -0500

    Estado del 90%

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,4 +1,6 @@
 import {createGlobalStyle} from 'styled-components'
+import PrimaryFont from  './utils/font/Hind-Light.woff';
+
 
 const GlobalStyles=createGlobalStyle`
 
@@ -7,21 +9,36 @@ const GlobalStyles=createGlobalStyle`
     height: 256px;
 }
 
-.ContainerText{
+.ContainerText p{
     margin:10px;
-    font-family:sans-serif;
-    font-size: 20px;
-}
+    font-family:'Hind';
+    font-size: 30px;
+    text-align: justify;
 
-p{
-    text-aling: justify;
 }
+.
 
 #principalTitle{
     font-size: 50px;
     color:rgb(226,70,43);
 }
 
+
+.ContainerImagen{
+    width: 50%;
+    margin-top: 5%;
+    margin-right: 2%;
+    margin-left: 2%;
+    height: 100%;
+    justify-content: center;
+}
+
+/*Fuentes*/
+@font-face {
+    font-family: 'Hind';
+    src: local('Hind'), url(${PrimaryFont}) format('woff');
+    font-style: normal;
+}
 `;
 
 export default GlobalStyles;
